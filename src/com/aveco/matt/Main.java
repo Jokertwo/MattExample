a1e8d594818c319e023adef926824678abad10fa
Jokertwo <p.lastovka@seznam.cz>
2019-07-10 11:19:48 +0200

    Repair my fault

--- src/com/aveco/matt/Main.java
+++ src/com/aveco/matt/Main.java
@@ -1,4 +1,36 @@
 package com.aveco.matt;
 
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
+import java.awt.FlowLayout;
+
 public class Main {
+
+
+    public Main(){
+        //visible window
+        JFrame frame = new JFrame("First window");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLocationRelativeTo(null);
+        frame.setSize(200,200);
+
+        //container for others component
+        JPanel container = new JPanel(new FlowLayout());
+
+        JButton buttonClick = new JButton("Click me");
+
+        //add button to container
+        container.add(buttonClick);
+
+        //add container to visible window
+        frame.add(container);
+        //set window visible
+        frame.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(Main::new);
+    }
 }
